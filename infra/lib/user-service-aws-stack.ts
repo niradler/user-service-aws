3335aef1337c991a63d275ca223384d0efa578b8
Nir Adler <niradler55@gmail.com>
2019-12-15 00:56:05 +0200

    register dynamodb

--- infra/lib/user-service-aws-stack.ts
+++ infra/lib/user-service-aws-stack.ts
@@ -1,10 +1,30 @@
 import cdk = require('@aws-cdk/core');
 import lambda = require('@aws-cdk/aws-lambda');
 import apigw = require('@aws-cdk/aws-apigateway');
+import dynamodb = require('@aws-cdk/aws-dynamodb');
 
 export class UserServiceAwsStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const usersTable = new dynamodb.Table(this, 'Users', {
+      partitionKey: { name: 'email', type: dynamodb.AttributeType.STRING }
+    });
+
+    const registerHandler = new lambda.Function(this, 'registerHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.asset('../functions'),
+      handler: 'register.handler',
+      environment: {
+        USERS_TABLE_NAME: usersTable.tableName
+      }
+    });
+
+    usersTable.grantReadWriteData(registerHandler);
+
+    new apigw.LambdaRestApi(this, 'registerEndpoint', {
+      handler: registerHandler
+    });
 
     const loginHandler = new lambda.Function(this, 'loginHandler', {
       runtime: lambda.Runtime.NODEJS_12_X,
@@ -15,5 +35,8 @@ export class UserServiceAwsStack extends cdk.Stack {
     new apigw.LambdaRestApi(this, 'loginEndpoint', {
       handler: loginHandler
     });
+
+
+
   }
 }
